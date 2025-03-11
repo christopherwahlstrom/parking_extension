a663fd46c002267df3a38920aeba6b1ee0f7ca7a
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2025-03-11 15:42:30 +0100

    Refactor JSON structure for persons and vehicles; update repository interface method names for clarity

--- parking_extension/shared/lib/src/repositories/repository_interface.dart
+++ parking_extension/shared/lib/src/repositories/repository_interface.dart
@@ -1,7 +1,7 @@
 abstract interface class RepositoryInterface<T> {
-  Future<T> create(T vehicle);
+  Future<T> create(T item);
   Future<List<T>> getAll();
   Future<T?> getById(String id);
-  Future<T> update(String id, T vehicle);
+  Future<T> update(String id, T item);
   Future<T> delete(String id);
 }
